6c441b022e6de308edea16a6dd88fe2c43bd7f6e
Anthony Adasik <anthonyadasiq@gmail.com>
2020-03-03 22:50:24 +0300

    fix: bug fixed

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -4,15 +4,17 @@ class VigenereCipheringMachine {
          this.flag = arg
    }
    encrypt(word, key) {
-      if(word === undefined || key === undefined) trow Error();
+      if(word === undefined || key === undefined) {
+         throw Error();
+      }
 
       let counter = 0;
       let result = '';
       word = word.toUpperCase();
       key = key.toUpperCase();
-      for(let i = 0; i < str.length; i++){
+      for(let i = 0; i < word.length; i++){
          if(/^[a-z]$/i.test(word[i]))
-         result += String.fromCharCode((str.charCodeAt(i) - 130 + key.charCodeAt((i - counter) % key.length)) % 26 + 65);
+         result += String.fromCharCode((word.charCodeAt(i) - 130 + key.charCodeAt((i - counter) % key.length)) % 26 + 65);
          else{
             counter++;
             result += word[i];
@@ -23,26 +25,25 @@ class VigenereCipheringMachine {
 
    decrypt(word, key) {
       if(word === undefined || key === undefined){
-          throw Error();
-      }
-      if(!this.flag){
-          word = word.split('').reverse().join('');
-      }
-      let counter = 0;
-      let result = '';
-      word = word.toUpperCase();
-      key = key.toUpperCase();
-      for(let i = 0; i < str.length; i++){
-          if(/^[a-z]$/i.test(word[i]))
+            throw Error();
+        }
+        if(!this.flag){
+            word = word.split('').reverse().join('');
+        }
+        let counter = 0;
+        let result = '';
+        word = word.toUpperCase();
+        key = key.toUpperCase();
+        for(let i = 0; i < word.length; i++){
+            if(/^[a-z]$/i.test(word[i]))
             result += String.fromCharCode((word.charCodeAt(i) + 26 - key.charCodeAt((i - counter) % key.length)) % 26 + 65);
-          else{
-             counter++;
-             result += word[i];
-          }
-      }
-      return result;
-      }
-   }
+            else{
+                counter++;
+                result += word[i];
+            }
+        }
+        return result;
+    }
 }
 
 module.exports = VigenereCipheringMachine;
